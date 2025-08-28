6439c8789b886303a5e963c37e7ea9bf13ab6ba4
João Pedro <jpedro1580@gmail.com>
2025-08-28 16:47:17 -0300

    feat: first data migration -> users and companies

---
+++ backend/db/migrations/001_create_user_table.sql
@@ -0,0 +1,30 @@
+CREATE TABLE company (
+    id uuid primary key,
+    name varchar not null, 
+    company_owner_id uuid not null,
+
+    created_at timestampz not null default CURRENT_TIMESTAMP,
+    updated_at timestampz not null default CURRENT_TIMESTAMP,
+    deleted_at timestampz
+    created_by uuid not null,
+    updated_by uuid not null
+)
+
+CREATE TABLE user(
+    id uuid primary key,
+    email varchar not null,
+    hashed_password varchar not null,
+    confirmed_at timestampz,
+    role varchar not null,
+
+    created_at timestampz not null default CURRENT_TIMESTAMP,
+    updated_at timestampz not null default CURRENT_TIMESTAMP,
+    deleted_at timestampz
+    created_by uuid not null,
+    updated_by uuid not null
+);
+
+---- create above / drop below ----
+
+drop table company;
+drop table user;
