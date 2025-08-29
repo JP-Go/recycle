ab10b5d4a15e7ff3e470815bddd9c56d0d02694b
João Pedro <jpedro1580@gmail.com>
2025-08-29 09:38:18 -0300

    chore: ignore env files

--- backend/db/migrations/001_create_user_and_company_table.sql
+++ backend/db/migrations/001_create_user_and_company_table.sql
@@ -1,30 +1,33 @@
-CREATE TABLE company (
-    id uuid primary key,
-    name varchar not null,
-    company_owner_id uuid not null,
+CREATE SCHEMA {{.schema}};
 
-    created_at timestampz not null default CURRENT_TIMESTAMP,
-    updated_at timestampz not null default CURRENT_TIMESTAMP,
-    deleted_at timestampz
+CREATE TABLE {{.schema}}.user(
+    id uuid primary key,
+    email varchar not null,
+    hashed_password varchar not null,
+    confirmed_at timestamp with time zone,
+    role varchar not null,
+
+    created_at timestamp with time zone not null default CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone not null default CURRENT_TIMESTAMP,
+    deleted_at timestamp with time zone,
     created_by uuid not null,
     updated_by uuid not null
 );
 
-CREATE TABLE user(
+CREATE TABLE {{.schema}}.company (
     id uuid primary key,
-    email varchar not null,
-    hashed_password varchar not null,
-    confirmed_at timestampz,
-    role varchar not null,
+    name varchar not null,
+    company_owner_id uuid references {{.schema}}.user(id) not null,
 
-    created_at timestampz not null default CURRENT_TIMESTAMP,
-    updated_at timestampz not null default CURRENT_TIMESTAMP,
-    deleted_at timestampz
+    created_at timestamp with time zone not null default CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone not null default CURRENT_TIMESTAMP,
+    deleted_at timestamp with time zone,
     created_by uuid not null,
     updated_by uuid not null
 );
 
 ---- create above / drop below ----
 
-drop table company;
-drop table user;
+drop table {{.schema}}.company;
+drop table {{.schema}}.user;
+drop schema {{.schema}} cascade;
